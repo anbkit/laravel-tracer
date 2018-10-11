0fbbc2d50db89866f3e29bc214ee60b51b355a51
An Nguyen <an@fuelcloud.com>
2018-10-11 13:18:43 +0700

    init first

---
+++ src/TracerServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+namespace FuelCloud\LaravelTracer;
+use Illuminate\Support\ServiceProvider;
+class TracerServiceProvider extends ServiceProvider{
+    public function register(){
+        
+    }
+
+    public function boot(){
+
+    }
+}
